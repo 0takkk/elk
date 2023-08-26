a2e5a196c8f03e12e872261cf3423acef0a0a882
0takkk <syt0719@naver.com>
2023-08-26 20:28:42 +0900

    주기적으로 log 실행

--- elk-spring1/src/main/java/com/example/elkspring1/ElkSpring1Application.java
+++ elk-spring1/src/main/java/com/example/elkspring1/ElkSpring1Application.java
@@ -1,13 +1,25 @@
 package com.example.elkspring1;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
-public class ElkSpring1Application {
+public class ElkSpring1Application implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ElkSpring1Application.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		int i = 0;
+		while(true) {
+			Thread.sleep(1000);
+			log.info("[elk-spring1] {}", ++i);
+		}
+	}
 }
--- elk-spring2/src/main/java/com/example/elkspring2/ElkSpring2Application.java
+++ elk-spring2/src/main/java/com/example/elkspring2/ElkSpring2Application.java
@@ -1,13 +1,25 @@
 package com.example.elkspring2;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
-public class ElkSpring2Application {
+public class ElkSpring2Application implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ElkSpring2Application.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		int i = 0;
+		while(true) {
+			Thread.sleep(2000);
+			log.info("[elk-spring2] {}", ++i);
+		}
+	}
 }
